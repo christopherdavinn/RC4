a167b17ef078a7e6919d25cbb9cfa513eef67c8d
leony-angela <leony.angela88@gmail.com>
2022-02-15 19:14:17 +0700

    Update lsfr.py

--- src/lsfr.py
+++ src/lsfr.py
@@ -2,7 +2,7 @@ def convert(s): # Convert string to ASCII
     return ([ord(c) for c in s])
 
 def getLsfr(kunci):
-    key = convert(kunci)
+    key = convert(kunci) #array of ASCII number
     binary = ''.join(format(i, '08b') for i in key)
     print(binary)
     l = len(binary)
@@ -13,7 +13,4 @@ def getLsfr(kunci):
         proses = str(int(binary[i]) ^ int(binary[i+3])) + binary[i:i+3] 
         output = output + proses
         outlist[:0] = output
-    return outlist
-
-#coba = getLsfr("halo")
-#print(coba)
+    return outlist #array of string of bytes ['0', '0', '1', '1', '1'] must be int(outlist[x])
